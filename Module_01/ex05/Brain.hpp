65b38afae46665c8ead0fb48f4f2bff8b93f0229
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-01 11:11:03 +0300

    ex05

---
+++ Module_01/ex05/Brain.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/01 11:00:29 by kbatwoma          #+#    #+#             */
+/*   Updated: 2021/03/01 11:10:47 by kbatwoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+# include <iostream>
+
+class Brain
+{
+    public:
+
+        Brain();
+        ~Brain();
+
+
+    private:
+
+        int volume;
+        int weight;
+        std::string dgfdg;
+
+};
+
+#endif
