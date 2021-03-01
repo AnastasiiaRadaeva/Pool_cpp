4b4bed54e33fe72215e878056f0877f9958bc416
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-01 15:38:45 +0300

    ex07_in_process_1

--- Module_01/ex07/main.cpp
+++ Module_01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 14:55:58 by kbatwoma          #+#    #+#             */
-/*   Updated: 2021/03/01 15:33:26 by kbatwoma         ###   ########.fr       */
+/*   Updated: 2021/03/01 15:38:38 by kbatwoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int	main(int argc, char **argv)
 	if (!argv[2] || !argv[3])
 		return (0); // обработать ошибку
 	std::ifstream	file(argv[1]);
-
+//find and replace - see in cplusplus
 		
 }
