1b6fa0240278eed894a35fe614ca533f40682a03
kbatwoma <kbatwoma@student.21-school.ru>
2021-03-01 23:55:46 +0300

    ex_07_in_process

--- Module_01/ex07/main.cpp
+++ Module_01/ex07/main.cpp
@@ -19,7 +19,20 @@ int	main(int argc, char **argv)
 		return (0); // нужно обработать эту ошибку
 	if (!argv[2] || !argv[3])
 		return (0); // обработать ошибку
-	std::ifstream	file(argv[1]);
+	
+	std::string		buff;
+	std::string		name_of_new_file = argv[1];
+	name_of_new_file.insert(name_of_new_file.length(), ".replace");
+	
+	std::ifstream	read_file(argv[1]);
+	std::ofstream	new_file(name_of_new_file);
+
+	while (std::getline(read_file, buff) > 0)
+	{
+		
+
+	}
+
 //find and replace - see in cplusplus
 		
 }
