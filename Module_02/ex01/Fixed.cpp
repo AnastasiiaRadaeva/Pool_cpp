c4dc544ed8fd2e9a2b55153ccc216ca184fea7ae
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-04 13:21:38 +0300

    ex01

--- Module_02/ex01/Fixed.cpp
+++ Module_02/ex01/Fixed.cpp
@@ -6,11 +6,15 @@
 /*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 15:40:26 by kbatwoma          #+#    #+#             */
-/*   Updated: 2021/03/03 14:59:09 by kbatwoma         ###   ########.fr       */
+/*   Updated: 2021/03/04 13:16:21 by kbatwoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+/*
+** Constructors
+*/
 
 Fixed::Fixed()
 {
@@ -18,9 +22,16 @@ Fixed::Fixed()
 	_fixed_point_num = 0;
 }
 
-Fixed::~Fixed()
+Fixed::Fixed(const int integer)
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "Int constructor called" << std::endl;
+	_fixed_point_num = integer << _num_of_fract_bits;
+}
+
+Fixed::Fixed(const float float_point_num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixed_point_num = (int)(roundf(float_point_num * ( 1 << _num_of_fract_bits)));
 }
 
 Fixed::Fixed(const Fixed& copy)
@@ -29,6 +40,19 @@ Fixed::Fixed(const Fixed& copy)
 	*this = copy;
 }
 
+/*
+** Destructor
+*/
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+/*
+** An assignation operator overload
+*/
+
 Fixed&	Fixed::operator=(const Fixed& fixed)
 {
 	std::cout << "Assignation operator called" << std::endl;
@@ -36,9 +60,12 @@ Fixed&	Fixed::operator=(const Fixed& fixed)
 	return (*this);
 }
 
+/*
+** Member functions
+*/
+
 int		Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (_fixed_point_num);
 }
 
@@ -47,7 +74,21 @@ void	Fixed::setRawBits(int const raw)
 	_fixed_point_num = raw;
 }
 
+float	Fixed::toFloat(void) const
+{
+	return ((float)_fixed_point_num / (float)(1 << _num_of_fract_bits));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (_fixed_point_num >> _num_of_fract_bits);
+}
+
+/*
+** Friend functions
+*/
+
 std::ostream&	operator<<(std::ostream &out, const Fixed& fixed)
 {
-	return (out << fixed.getRawBits());
+	return (out << fixed.toFloat());
 }
