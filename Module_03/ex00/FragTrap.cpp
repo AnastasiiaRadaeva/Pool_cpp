a53aef4928580f5242f036ff3cdd0d1a9e109bef
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-11 16:59:13 +0300

    ex00

--- Module_03/ex00/FragTrap.cpp
+++ Module_03/ex00/FragTrap.cpp
@@ -6,26 +6,48 @@
 /*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 16:02:03 by kbatwoma          #+#    #+#             */
-/*   Updated: 2021/03/04 18:19:34 by kbatwoma         ###   ########.fr       */
+/*   Updated: 2021/03/11 16:17:12 by kbatwoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fragtrap.hpp"
 
-FragTrap::FragTrap()
+FragTrap::FragTrap() : _name("FragTrap"), _hit_points(100), _max_hit_points(100), _energy_points(100), _max_energy_points(100), _level(1), _melee_attack_damage(30), _ranged_attack_damage(20), _super_attack_damage(25), _armor_damage_reduction(5)
 {
-	
+	std::cout << "< FR4G-TP " << _name << " has arrived >" << std::endl;
+	std::cout << "< FR4G-TP " << _name << " > Let's get this party started!" << std::endl;
+	std::cout << "| Hit points: " << _hit_points << std::endl;
+	std::cout << "| Energy points: " << _energy_points << std::endl;
 }
 
 FragTrap::FragTrap(FragTrap const &fragtrap)
 {
 	*this = fragtrap;
-	_name = fragtrap.getName();
+	_name = fragtrap._name;
+	_hit_points = fragtrap._hit_points;
+	_max_hit_points = fragtrap._max_hit_points;
+	_energy_points = fragtrap._energy_points;
+	_max_energy_points = fragtrap._max_energy_points;
+	_level = fragtrap._level;
+	_melee_attack_damage = fragtrap._melee_attack_damage;
+	_ranged_attack_damage = fragtrap._ranged_attack_damage;
+	_super_attack_damage = fragtrap._super_attack_damage;
+	_armor_damage_reduction = fragtrap._armor_damage_reduction;
 }
 
 FragTrap& FragTrap::operator=(FragTrap const &fragtrap)
 {
-	_name = fragtrap.getName();
+	_name = fragtrap._name;
+	_hit_points = fragtrap._hit_points;
+	_max_hit_points = fragtrap._max_hit_points;
+	_energy_points = fragtrap._energy_points;
+	_max_energy_points = fragtrap._max_energy_points;
+	_level = fragtrap._level;
+	_melee_attack_damage = fragtrap._melee_attack_damage;
+	_ranged_attack_damage = fragtrap._ranged_attack_damage;
+	_super_attack_damage = fragtrap._super_attack_damage;
+	_armor_damage_reduction = fragtrap._armor_damage_reduction;
+	
 	return (*this);
 }
 
@@ -35,33 +57,106 @@ FragTrap::~FragTrap()
 	std::cout << "< FR4G-TP " << _name << " left us >" << std::endl;
 }
 
-FragTrap::FragTrap(std::string name) : _name(name)
+FragTrap::FragTrap(std::string name) : _name(name), _hit_points(100), _max_hit_points(100), _energy_points(100), _max_energy_points(100), _level(1), _melee_attack_damage(30), _ranged_attack_damage(20), _super_attack_damage(25), _armor_damage_reduction(5)
 {
 	std::cout << "< FR4G-TP " << _name << " has arrived >" << std::endl;
 	std::cout << "< FR4G-TP " << _name << " > Let's get this party started!" << std::endl;
+	std::cout << "| " << _name << " | Hit points: " << _hit_points << std::endl;
+	std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
 }
 
+/*
+** Member functions
+*/
 
-void	FragTrap::rangedAttack(std::string const & target)
+int		FragTrap::rangedAttack(std::string const & target)
 {
-	std::cout << "< FR4G-TP " << _name << " attacks " << target << " at range, causing " << _ranged_attack_damage << " points of damage!" << std::endl;
+	if (_energy_points < _ranged_attack_damage)
+	{
+		std::cout << "< FR4G-TP " << _name << " trying to attack " << target << ", but its energy is running out >" << std::endl;
+		std::cout << " -- Failed attempt -- " << std::endl;
+		std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+		return (0);
+	}
+	else
+	{
+		_energy_points -= _ranged_attack_damage;
+		std::cout << "< FR4G-TP " << _name << " attacks " << target << " at range, causing " << _ranged_attack_damage << " points of damage! >" << std::endl;
+		std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+		return (_ranged_attack_damage);
+	}
 }
 
-void	FragTrap::meleeAttack(std::string const & target)
+int		FragTrap::meleeAttack(std::string const & target)
 {
-	std::cout << "< FR4G-TP " << _name << " attacks " << target << " in melee , causing " << _melee_attack_damage << " points of damage!" << std::endl;
+	if (_energy_points < _melee_attack_damage)
+	{
+		std::cout << "< FR4G-TP " << _name << " trying to attack " << target << ", but its energy is running out >" << std::endl;
+		std::cout << " -- Failed attempt -- " << std::endl;
+		std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+		return (0);
+	}
+	else
+	{
+		_energy_points -= _melee_attack_damage;
+		std::cout << "< FR4G-TP " << _name << " attacks " << target << " in melee , causing " << _melee_attack_damage << " points of damage! >" << std::endl;
+		std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+		return (_melee_attack_damage);
+	}
 }
 
 void	FragTrap::takeDamage(unsigned int amount)
 {
+	std::cout << "| " << _name << " | Armor damage reduction: " << _armor_damage_reduction << std::endl;	
+	int copy_amount = amount;
+	amount = copy_amount < _armor_damage_reduction ? 0 : amount - _armor_damage_reduction;
 	_hit_points = ((_hit_points - amount) < 0) ? 0 : (_hit_points - amount);
 	std::cout << "< FR4G-TP " << _name << " took " << amount << " damage >" << std::endl;
+	std::cout << "| " << _name << " | Hit points: " << _hit_points << std::endl;
 }
 
 void	FragTrap::beRepaired(unsigned int amount)
 {
-	_hit_points = ((_hit_points + amount) > _max_hit_points) ? _max_hit_points : (_hit_points + amount);
-	std::cout << "< FR4G-TP " << _name << " was repaired by " << amount << " points >" << std::endl;
+	int	energy_amount = amount / 2;
+
+	_hit_points += amount;
+	_hit_points = _hit_points > _max_hit_points ? _max_hit_points : _hit_points;
+	_energy_points += energy_amount;
+	_energy_points = _energy_points > _max_energy_points ? _max_energy_points : _energy_points;	
+
+	std::cout << "< FR4G-TP " << _name << " was repaired by " << amount << " hit points" << std::endl;
+	std::cout << "and " << energy_amount << " energy points >" << std::endl;
+	std::cout << "| " << _name << " | Hit points: " << _hit_points << std::endl;
+	std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+}
+
+int		FragTrap::vaulthunter_dot_exe(std::string const & target)
+{
+	srand(time(NULL));
+	int		i = rand()%5;
+	
+	std::string	super_attacks[5];
+	super_attacks[0] = "|Kick Him While He's Down|";
+	super_attacks[1] = "|I Am Rubber, You Are Glue|";
+	super_attacks[2] = "|Livin' Near the Edge|";
+	super_attacks[3] = "|Rainbow Coolant|";
+	super_attacks[4] = "|Moon Cosmic Power|";
+
+	std::cout << "!!! SUPER ATACK !!!" << std::endl;
+	if (_energy_points < _super_attack_damage)
+	{
+		std::cout << "< FR4G-TP " << _name << " trying to attack " << target << ", but its energy is running out >" << std::endl;
+		std::cout << " -- Failed attempt -- " << std::endl;
+		std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+		return (0);
+	}
+	else
+	{
+		_energy_points -= _super_attack_damage;
+		std::cout << "< FR4G-TP " << _name << " attacks " << target << " with a super attak... " << super_attacks[i] << ", causing " << _super_attack_damage << " points of damage! >" << std::endl;
+		std::cout << "| " << _name << " | Energy points: " << _energy_points << std::endl;
+		return (_super_attack_damage);
+	}
 }
 
 std::string	FragTrap::getName(void) const
--- Module_03/ex00/Fragtrap.hpp
+++ Module_03/ex00/Fragtrap.hpp
@@ -6,14 +6,16 @@
 /*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 16:01:51 by kbatwoma          #+#    #+#             */
-/*   Updated: 2021/03/04 17:37:30 by kbatwoma         ###   ########.fr       */
+/*   Updated: 2021/03/11 16:57:10 by kbatwoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRAGTRAP_HPP
 # define FRAGTRAP_HPP
 
-# include <iostream>
+# include <iostream> 
+# include <stdlib.h>
+# include <time.h> //for time
 
 class FragTrap
 {
@@ -26,26 +28,26 @@ class FragTrap
 
 		FragTrap(std::string name);
 
-		void	rangedAttack(std::string const & target);
-		void	meleeAttack(std::string const & target);
+		int		rangedAttack(std::string const & target);
+		int 	meleeAttack(std::string const & target);
 		void	takeDamage(unsigned int amount);
 		void	beRepaired(unsigned int amount);
+		int 	vaulthunter_dot_exe(std::string const & target);
 
 		std::string	getName(void) const;
 
 	private:
 
 		std::string	_name;
-		
-		int	_hit_points = 100;
-		int	_max_hit_points = 100;
-		int	_energy_points = 100;
-		int	_max_energy_points = 100;
-		int	_level = 1;
-		int	_melee_attack_damage = 30;
-		int	_ranged_attack_damage = 20;
-		int	_armor_damage_reduction = 5;
-
+		int			_hit_points;
+		int			_max_hit_points;
+		int			_energy_points;
+		int			_max_energy_points;
+		int			_level;
+		int			_melee_attack_damage;
+		int			_ranged_attack_damage;
+		int			_super_attack_damage;
+		int			_armor_damage_reduction;
 };
 
 #endif
