d50d3fa6e3eea1e73272c31bef99aed3071785f1
kbatwoma <kbatwoma@student.21-school.ru>
2021-03-18 23:55:58 +0300

    ex01_1

--- Module_05/ex01/Form.hpp
+++ Module_05/ex01/Form.hpp
@@ -6,8 +6,33 @@ class Form
 {
     public:
 
+        Form();
+        Form(std::string name, int  grade_for_sign, int grade_for_ex);
+        Form(Form const &);
+        Form &operator=(Form const &);
+        ~Form();
+
+        std::string const   &getName() const;
+        int                 getGradeSign() const;
+        int                 getGradeEx() const;
+        bool                getStatus() const;
+
+        class GradeTooHighException : public std::exception
+        {
+            const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            const char* what() const throw();
+        };
+
     private:
 
+        std::string const   _name;
+        bool                _status;
+        int const           _grade_for_sign;
+        int const           _grede_for_execute;
 };
 
 #endif
