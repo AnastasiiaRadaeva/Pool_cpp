addb2d54039082b6fe08e649a71f23d1694dcf82
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-21 12:56:31 +0300

    ex03_final_I_bel

--- Module_05/ex03/Intern.cpp
+++ Module_05/ex03/Intern.cpp
@@ -23,45 +23,62 @@ static Form    *cr_ShrubberyCreationForm(std::string const &target)
 ** Main Part
 */
 
-Intern::Intern() : _num_of_temp(3)
+Intern::Intern() : _num_of_temp(3) //_num_of_temp is always 3
 {
-    _templates = new Templates[_num_of_temp];
+    _templates = new Templates*[_num_of_temp];
 
-    _templates[0].form_name = "presidential pardon";
-    _templates[0].create_form = &cr_PresidentialPardonForm;
-    _templates[1].form_name = "robotomy request";
-    _templates[1].create_form = &cr_RobotomyRequestForm;
-    _templates[2].form_name = "shrubbery creation";
-    _templates[2].create_form = &cr_ShrubberyCreationForm;
+    int i = -1;
+    while (++i < _num_of_temp)
+        _templates[i] = new Templates;
+    _templates[0]->form_name = "presidential pardon";
+    _templates[0]->create_form = &cr_PresidentialPardonForm;
+    _templates[1]->form_name = "robotomy request";
+    _templates[1]->create_form = &cr_RobotomyRequestForm;
+    _templates[2]->form_name = "shrubbery creation";
+    _templates[2]->create_form = &cr_ShrubberyCreationForm;
 }
 
 Intern::Intern(Intern const &intern) : _num_of_temp(intern._num_of_temp)
 {
-    _templates = new Templates[_num_of_temp];
-    
-    _templates[0].form_name = "presidential pardon";
-    _templates[0].create_form = &cr_PresidentialPardonForm;
-    _templates[1].form_name = "robotomy request";
-    _templates[1].create_form = &cr_RobotomyRequestForm;
-    _templates[2].form_name = "shrubbery creation";
-    _templates[2].create_form = &cr_ShrubberyCreationForm;
+    _templates = new Templates*[_num_of_temp];
+
+    int i = -1;
+    while (++i < _num_of_temp)
+        _templates[i] = new Templates;
+    _templates[0]->form_name = "presidential pardon";
+    _templates[0]->create_form = &cr_PresidentialPardonForm;
+    _templates[1]->form_name = "robotomy request";
+    _templates[1]->create_form = &cr_RobotomyRequestForm;
+    _templates[2]->form_name = "shrubbery creation";
+    _templates[2]->create_form = &cr_ShrubberyCreationForm;
 }
 
 Intern &Intern::operator=(Intern const &intern)
 {
     if (this != &intern)
     {
-        delete [] _templates;
-        _templates = new Templates[_num_of_temp];
         int i = -1;
         while (++i < _num_of_temp)
-            _templates[i] = intern._templates[i];
+            delete _templates[i];
+        delete [] _templates;
+
+        _templates = new Templates*[_num_of_temp];
+        i = -1;
+        while (++i < _num_of_temp)
+        {
+            _templates[i] = 0;
+            if (intern._templates[i])
+                _templates[i] = new Templates(*(intern._templates[i]));
+        }
     }
     return (*this);
 }
 
 Intern::~Intern()
 {
+    int i = -1;
+    while (++i < _num_of_temp)
+        delete _templates[i];
     delete [] _templates;
 }
 
@@ -75,10 +92,10 @@ Form    *Intern::makeForm(std::string const &name_of_form, std::string const &ta
 
     int i = -1;
     while (++i < 3)
-        if (_templates[i].form_name == name_of_form)
+        if (_templates[i]->form_name == name_of_form)
         {
-            new_form = _templates[i].create_form(target);
-            std::cout << "Intern creates " << _templates[i].form_name << std::endl;
+            new_form = _templates[i]->create_form(target);
+            std::cout << "Intern creates " << _templates[i]->form_name << std::endl;
             return (new_form);
         }
     std::cerr << "This form is not known" << std::endl;
--- Module_05/ex03/Intern.hpp
+++ Module_05/ex03/Intern.hpp
@@ -24,7 +24,7 @@ class Intern
 
     private:
 
-        Templates   *_templates;
+        Templates   **_templates;
         int const   _num_of_temp;
 };
 
