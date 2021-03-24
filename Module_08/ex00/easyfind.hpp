f5b68cb6ef91ec3a35c16832e422737e3a384222
kbatwoma <kbatwoma@student.21-school.ru>
2021-03-24 23:59:46 +0300

    mod_8_start

---
+++ Module_08/ex00/easyfind.hpp
@@ -0,0 +1,12 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+
+template <template < int, class Alloc = allocator<int> > class T>
+T::iterator easyfind(T base, int first_occ)
+{
+
+}
+
+#endif
