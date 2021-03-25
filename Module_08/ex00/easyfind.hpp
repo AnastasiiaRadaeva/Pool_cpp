30e03ff50465ab3b2665708ca7052a4c464b3204
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-25 12:37:17 +0300

    ex00_start

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -1,12 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 10:18:35 by kbatwoma          #+#    #+#             */
+/*   Updated: 2021/03/25 12:35:43 by kbatwoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <algorithm>
+# include <map>
 
-template <template < int, class Alloc = allocator<int> > class T>
-T::iterator easyfind(T base, int first_occ)
+class OccurenceNotFound : public std::exception
 {
+    const char *what() const throw() 
+    {
+        return ("Error: Occurence of the second parametr in the first parametr is not found.");
+    }
+};
 
+template <typename T>
+typename T::iterator easyfind(T &base, int first_occur)
+{
+    typename T::iterator it_occur;
+    if ((it_occur = std::find(std::begin(base), std::end(base), first_occur)) != std::end(base))
+        return (it_occur);
+    throw (OccurenceNotFound()); 
+}
+
+template <typename key, typename value>
+typename std::map<int, int>::iterator easyfind(std::map<int, int> &map_, int first_occur)
+{
+    return (map_.find(first_occur));
+    throw (OccurenceNotFound());
+}
+
+template <typename key, typename value>
+typename std::multimap<int, int>::iterator easyfind(std::multimap<int, int> &multimap_, int first_occur)
+{
+    return (multimap_.find(first_occur));
+    throw (OccurenceNotFound());
 }
 
 #endif
---
+++ Module_08/ex00/main.cpp
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 10:39:58 by kbatwoma          #+#    #+#             */
+/*   Updated: 2021/03/25 12:35:48 by kbatwoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "easyfind.hpp"
+# include <vector>
+# include <list>
+
+
+int main()
+{
+    std::cout << "--- List ---" << std::endl;
+    std::list<int>      list_1;
+    list_1.push_back(1);
+    list_1.push_back(5);
+    list_1.push_back(9);
+    list_1.push_back(76);
+    list_1.push_back(91);
+    std::list<int>::iterator it_list;
+    it_list = easyfind(list_1, 5);
+    std::cout << *it_list << std::endl;
+    try
+    {
+        it_list = easyfind(list_1, 100);
+        std::cout << *it_list << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "--- Vector ---" << std::endl;
+    std::vector<int>    vector_1;
+    vector_1.push_back(3);
+    vector_1.push_back(7);
+    vector_1.push_back(11);
+    vector_1.push_back(21);
+    vector_1.push_back(42);
+    std::vector<int>::iterator it_vector;
+    it_vector = easyfind(vector_1, 21);
+    std::cout << *it_vector << std::endl;
+    try
+    {
+        it_vector = easyfind(vector_1, 100);
+        std::cout << *it_vector << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
+    std::cout << std::endl;
+
+    std::cout << "--- Map ---" << std::endl;
+    std::map<int, int> map_1;
+    map_1[1] = 1;
+    map_1[2] = 32;
+    map_1[3] = 58;
+    std::map<int, int>::iterator it_map;
+    it_map = easyfind(map_1, 3);
+    std::cout << it_map->first << ", " << it_map->second << std::endl;
+    try
+    {
+        it_map = easyfind(map_1, 100);
+    std::cout << it_map->first << ", " << it_map->second << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
+    std::cout << std::endl;
+
+    // std::cout << "--- Multimap ---" << std::endl;
+    // std::multimap<int, int> multimap_1;
+    // typedef std::pair <int, int> int_pair;
+    // multimap_1.insert(int_pair(1, 1));
+    // multimap_1.insert(int_pair(2, 4));
+    // multimap_1.insert(int_pair(2, 7));
+    // std::multimap<int, int>::iterator it_multimap;
+    // it_multimap = easyfind(multimap_1, 2);
+    // std::cout << it_multimap->first << ", " << it_multimap->second << std::endl;
+    // try
+    // {
+    //     it_map = easyfind(multimap_1, 100);
+    // std::cout << it_multimap->first << ", " << it_multimap->second << std::endl;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // return (0);
+    // std::cout << std::endl;
+
+    return (0);
+}
+
+//clang++ -Wall -Wextra -Werror *.cpp 
